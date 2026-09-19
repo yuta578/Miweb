318a218a893c35a3c6b1821d86edcbd25c6d08fa
yuta578 <guevarajhoan47@gmail.com>
2026-09-19 15:28:22 -0500

    test: add unit tests for Calculator and .gitignore configuration

--- src/test.py
+++ src/test.py
@@ -1,7 +1,13 @@
-# testeo
-def test_example():
-    assert 1 + 1 == 2
+from main import Calculator
+
+def test_sums_2_numbers():
+    assert Calculator().suma(2, 2) == 4
+
+def test_resta_2_numbers():
+    assert Calculator().resta(5, 3) == 2
+
 
 if __name__ == "__main__":
-    test_example()
+    test_sums_2_numbers()
+    test_resta_2_numbers()
     print("¡Todas las pruebas pasaron correctamente!")
