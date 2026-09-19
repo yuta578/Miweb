02743ee3980561695313e7b7d1f4ad709df3c8b8
yuta578 <guevarajhoan47@gmail.com>
2026-09-19 15:52:49 -0500

    test: add unit tests for Calculator addition and subtraction

--- src/test.py
+++ src/test.py
@@ -5,9 +5,3 @@ def test_sums_2_numbers():
 
 def test_resta_2_numbers():
     assert Calculator().resta(5, 3) == 2
-
-
-if __name__ == "__main__":
-    test_sums_2_numbers()
-    test_resta_2_numbers()
-    print("¡Todas las pruebas pasaron correctamente!")
